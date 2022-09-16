5a8f22608eabbf741dab66da376f78da6f2a2e40
grahnavard <gholamali.rahnavard@gmail.com>
2022-09-16 19:01:59 -0400

    plot to accpet ''

--- btest/blockplot.py
+++ btest/blockplot.py
@@ -97,6 +97,7 @@ class Table:
                     self.colheads = values
                 else:
                     self.rowheads.append(rowhead)
+                    values = [np.nan if x=='' else x for x in values]
                     self.data.append(list(map(float, values)))
         self.data = np.array(self.data)
         self.update()
@@ -185,10 +186,11 @@ def load_order_table(p_table, associations, p_tree = None):
 
 def load_associations(path, largest=None, strongest=100, orderby='similarity'):
     pairs = []
-    dic_order = {'pvalue': 3, 'similarity': 4, 'qvalue': 5}
+    dic_order = {'pvalue': 3, 'similarity': 4, 'qvalue': 6}
+
     with open(path) as fh:
         for row in csv.reader(fh, dialect="excel-tab"):
-            if row[0] !='':
+            if row[0] != '' and row[dic_order[orderby]] != '':
                 pairs.append([row[0], row[1].split(";"), row[2].split(";"), row[3], row[4], row[6]])
     if largest is not None and strongest is not None:
         sys.exit("Can only specify one of LARGEST and STRONGEST")
